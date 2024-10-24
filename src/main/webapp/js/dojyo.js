580a9fe9a6589f98932e4bdb9ccb9c26299156e6
esegamesshu <152826080+esegamesshu@users.noreply.github.com>
2024-10-24 12:30:58 +0900

    js作成

---
+++ src/main/webapp/js/dojyo.js
@@ -0,0 +1,59 @@
+function national_language() {
+	let link1 = document.getElementById('link1');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let url1 = '#';
+	let url2 = '#';
+	let url3 = '#';
+	link1.setAttribute('href', url1);
+	link2.setAttribute('href', url2);
+	link3.setAttribute('href', url3);
+}
+
+function mathematics() {
+	let link1 = document.getElementById('link1');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let url1 = '#';
+	let url2 = '#';
+	let url3 = '#';
+	link1.setAttribute('href', url1);
+	link2.setAttribute('href', url2);
+	link3.setAttribute('href', url3);
+}
+
+function english() {
+	let link1 = document.getElementById('link1');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let url1 = '#';
+	let url2 = '#';
+	let url3 = '#';
+	link1.setAttribute('href', url1);
+	link2.setAttribute('href', url2);
+	link3.setAttribute('href', url3);
+}
+
+function science() {
+	let link1 = document.getElementById('link1');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let url1 = '#';
+	let url2 = '#';
+	let url3 = '#';
+	link1.setAttribute('href', url1);
+	link2.setAttribute('href', url2);
+	link3.setAttribute('href', url3);
+}
+
+function society() {
+	let link1 = document.getElementById('link1');
+	let link2 = document.getElementById('link2');
+	let link3 = document.getElementById('link3');
+	let url1 = '#';
+	let url2 = '#';
+	let url3 = '#';
+	link1.setAttribute('href', url1);
+	link2.setAttribute('href', url2);
+	link3.setAttribute('href', url3);
+}
