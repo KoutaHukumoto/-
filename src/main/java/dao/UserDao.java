e648c39c48da5f7ba5814df6d369a0cfe1b05033
esegamesshu <152826080+esegamesshu@users.noreply.github.com>
2024-12-05 11:31:21 +0900

    aaa

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -76,7 +76,7 @@ public class UserDao extends BaseDao {
                             rs.getInt("characterid"),       
                             rs.getInt("hp"),        
                             rs.getInt("attack"),    
-                            rs.getInt("defence"),  
+                            rs.getInt("defense"),  
                             rs.getInt("speed"),
                             rs.getInt("itemid"),
                             rs.getInt("dungeonid")
